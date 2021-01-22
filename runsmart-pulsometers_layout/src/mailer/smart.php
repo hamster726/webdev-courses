7c923aa27af9e66cc07898bce70911bdf2f8e4d3
Hamster726 <yevhen.shaparenko@gmail.com>
2021-01-22 18:41:37 +0200

    Updated and fixed PHPmailer

--- runsmart-pulsometers_layout/src/mailer/smart.php
+++ runsmart-pulsometers_layout/src/mailer/smart.php
@@ -1,44 +1,57 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
-// $mail->SMTPDebug = 3;                                // Enable verbose debug output
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'hamaster726@gmail.com';                     // SMTP username
+    $mail->Password   = 'hufwbcbtcdwogbpz';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-$mail->isSMTP();                                        // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                                 // Enable SMTP authentication
-$mail->Username = '';                                   // Наш логин
-$mail->Password = '';                                   // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                              // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                      // TCP port to connect to
+    //Recipients
+    $mail->setFrom('hamaster726@gmail.com', 'SmartPulse');
+    $mail->addAddress('sahak24796@dashseat.com', 'Client');     // Add a recipient
+    $mail->addAddress('hamaster726@gmail.com');               // Name is optional
+//    $mail->addReplyTo('info@example.com', 'Information');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
 
-$mail->setFrom('hamaster726@gmail.com', 'SmartPulse');               // От кого письмо
-$mail->addAddress('sahak24796@dashseat.com');                           // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                              // Set email format to HTML
+    // Attachments
+//    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
-$mail->Subject = 'Данные';
-$mail->Body    = '
-		Пользователь оставил данные <br> 
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'SmartPulse';
+    $mail->Body    = '
+	Пользователь оставил данные <br> 
 	Имя: ' . $name . ' <br>
 	Номер телефона: ' . $phone . '<br>
 	E-mail: ' . $email . '';
 
-if(!$mail->send()) {
-    return false;
-} else {
-    return true;
-}
+//    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-?>
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
