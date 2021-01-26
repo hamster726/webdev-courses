85532d67cef729faffbf0ec3b32be5461f4452e6
Hamster726 <114generals>
2021-01-26 21:10:03 +0200

    Quiet... :)

--- runsmart-pulsometers_layout/src/mailer/smart.php
+++ runsmart-pulsometers_layout/src/mailer/smart.php
@@ -20,10 +20,10 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'hamaster726@gmail.com';                     // SMTP username
-    $mail->Password   = 'hufwbcbtcdwogbpz';                               // SMTP password
+    $mail->Host       = 'smtp.gmail.com';                            // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                        // Enable SMTP authentication
+    $mail->Username   = '';                                          // SMTP username
+    $mail->Password   = '';                                          // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
